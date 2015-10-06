820e15da8fa14b0be068588c089e2b6cc21d5093
Kephren Newton <kephren.newton@gmail.com>
2015-10-05 20:09:28 -0500

    Added the two original de-cycle algos back in

--- alphabet-cipher/src/alphabet_cipher/coder_kephren.clj
+++ alphabet-cipher/src/alphabet_cipher/coder_kephren.clj
@@ -17,7 +17,7 @@
                                  (a->idx %2)))
                  left right)))
 
-;the-michael-willis-de-cycle-plus
+;the-michael-de-cycle-plus
 (defn de-cycle [raw]
   (loop [size 1]
     (let [keyword (seq->str (take size raw))
@@ -25,6 +25,28 @@
       (if (or (= keyword tail)
               (= raw (str keyword tail) ))
         keyword
+        (recur (inc size))))))
+
+(defn de-cycle-prototype [raw]
+  (loop [front ""
+         keyword raw
+         back ""]
+    (let [next-front (seq->str (take (inc (count front)) keyword))
+          next-back (seq->str (take-last (inc (count back)) keyword))]
+      (println "->" next-front keyword next-back)
+      (cond
+        (= next-front keyword) keyword
+        (and (in? next-front next-back)
+             (not= front next-back)
+             ) (recur front (seq->str (drop-last (count next-back) keyword)) "")
+        :else (recur next-front keyword next-back)))))
+
+(defn dumb-de-cycle [raw]
+  (let [full-size (count raw)]
+    (loop [size 1]
+      (if (= (seq->str (->> raw (take size) (cycle) (take full-size)))
+             raw)
+        (seq->str (take size raw))
         (recur (inc size))))))
 
 (defn encode [keyword message]
