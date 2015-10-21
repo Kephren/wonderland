fd4771d91f3567d3ceec7313904c5d2ce55c8ec8
Kephren Newton <kephren.newton@gmail.com>
2015-10-20 20:38:12 -0500

    First stab at wrong card game included

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -4,10 +4,47 @@
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
 (def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+  (for [rank ranks
+        suit suits]
+    [rank suit]))
 
-(defn play-round [player1-card player2-card])
+(defn score [card] (.indexOf cards card))
 
-(defn play-game [player1-cards player2-cards])
+(def shuffled (shuffle cards))
+(def player1-hand (take-nth 2 shuffled))
+(def player2-hand (take-nth 2 (drop 1 shuffled)))
+
+(defn play-round [p1-card p2-card]
+  (if (> (score p1-card) (score p2-card))
+    :p1ayer1-won
+    :player2-won))
+
+
+(defn play-game [p1-cards p2-cards]
+  (let [[p1-card & p1-deck] p1-cards
+        [p2-card & p2-deck] p2-cards
+        reward (shuffle [p1-card p2-card])]
+    (cond
+      (empty? p1-cards) "Player 2 Wins!"
+      (empty? p2-cards) "Player 1 Wins!"
+      ;reward player1
+      (= (play-round p1-card p2-card)
+         :player1-won)
+      (recur (concat p1-deck reward) p2-deck)
+      ;reward player2
+      :else
+      (recur p1-deck (concat p2-deck reward)))))
+
+;; Not the same game.
+(comment
+  (defn play-wrong-game [player1-cards player2-cards]
+    (let [score (->> (map play-round player1-cards player2-cards)
+                     (frequencies))
+          winner (->> score
+                      (sort-by val >)
+                      (ffirst))]
+      (if (= (:p1 score)
+             (:p2 score))
+        :draw
+        winner))))
+
